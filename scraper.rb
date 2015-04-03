f0eae013ebfb0f2c7e7010eab3da9e1f0dfcd585
Matthew Landauer <matthew@oaf.org.au>
2015-04-04 09:38:02 +1100

    Handle when there are lots of products in a category

--- scraper.rb
+++ scraper.rb
@@ -4,6 +4,7 @@
 require "base64"
 require "dotenv"
 require "rest_client"
+require "yaml"
 
 # The "stupidest encoder in the world"
 class Encoder
@@ -45,8 +46,20 @@ class ProductApi
     JSON.parse(RestClient.get "#{BASE}/assets/categories.json", "Api-Key" => api_key)
   end
 
-  def products_in_category(id, page = 1)
-    JSON.parse(RestClient.get "#{BASE}/api/products", params: {category: id, page: page}, "Api-Key" => api_key)
+  # It looks like there's a max of 20 requests per page
+  def products_in_category_paged(id, page)
+    JSON.parse(RestClient.get "#{BASE}/api/products", params: {category: id, page: page}, "Api-Key" => api_key)["Items"]
+  end
+
+  def products_in_category(id)
+    page = 0
+    products = []
+    begin
+      page += 1
+      a = products_in_category_paged(id, page)
+      products += a
+    end while a.count == 20
+    products
   end
 
   def product_info(id)
@@ -69,6 +82,27 @@ raise unless e.decode("MTlANzc3Nz08QDo7OkA3") == "09343956000092"
 raise unless e.decode(e.encode("09343956000092")) == "09343956000092"
 
 api = ProductApi.new
-#p api.categories
 #p api.products_in_category("10000613")
-p api.product_info("09343956000092")
+#p api.product_info("09343956000092")
+
+def leaf_category_ids(categories)
+  ids = []
+  categories.each do |category|
+    #p category
+    if category["Children"].empty?
+      ids << category["Code"]
+    else
+      ids += leaf_category_ids(category["Children"])
+    end
+  end
+  ids.sort
+end
+
+c = api.categories
+puts c.to_yaml
+p leaf_category_ids(c["Children"])
+
+#puts api.categories.to_yaml
+
+puts api.products_in_category("10000262").to_yaml
+#puts api.products_in_category_paged("10000262", 1).to_yaml
